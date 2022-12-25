708a64b997061b01c574d41ed590d34b1ab631b2
Atsuma <asd463291@gmail.com>
2022-12-25 23:54:57 +0800

    feat: sprite swapping

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,12 @@
-import Sprite from './objects/Sprite.js'
-import Player from './objects/Player.js'
-import { keysPressed, addEventListener } from './features/eventListener.js'
-import { collisionsLevel1 } from './data/collisions.js'
-import parse2D from './utils/parse2D.js'
-import createObjectsFrom2D from './utils/createObjectsFrom2D.js'
+import Sprite from "./objects/Sprite.js"
+import Player from "./objects/Player.js"
+import { keysPressed, addEventListener } from "./features/eventListener.js"
+import { collisionsLevel1 } from "./data/collisions.js"
+import parse2D from "./utils/parse2D.js"
+import createObjectsFrom2D from "./utils/createObjectsFrom2D.js"
 
-const canvas = document.querySelector('canvas')
-const context = canvas.getContext('2d')
+const canvas = document.querySelector("canvas")
+const context = canvas.getContext("2d")
 canvas.width = 64 * 16 // 1024
 canvas.height = 64 * 9 // 576
 
@@ -15,13 +15,39 @@ const collisionBlocks = parsedCollisions.createObjectsFrom2D()
 
 const backgroundLevel1 = new Sprite({
   position: { x: 0, y: 0 },
-  imageSrc: './assets/img/backgrounds/level1.png',
+  imageSrc: "./assets/img/backgrounds/level1.png",
 })
 
 const player = new Player({
   collisionBlocks,
-  imageSrc: './assets/img/king/idle.png',
+  imageSrc: "./assets/img/king/idle.png",
   frameRate: 11,
+  animations: {
+    idleRight: {
+      frameRate: 11,
+      frameBuffer: 2,
+      loop: true,
+      imageSrc: "./assets/img/king/idle.png",
+    },
+    idleLeft: {
+      frameRate: 11,
+      frameBuffer: 2,
+      loop: true,
+      imageSrc: "./assets/img/king/idleLeft.png",
+    },
+    runRight: {
+      frameRate: 8,
+      frameBuffer: 4,
+      loop: true,
+      imageSrc: "./assets/img/king/runRight.png",
+    },
+    runLeft: {
+      frameRate: 8,
+      frameBuffer: 4,
+      loop: true,
+      imageSrc: "./assets/img/king/runLeft.png",
+    },
+  },
 })
 
 function animate() {
@@ -29,8 +55,22 @@ function animate() {
   collisionBlocks.forEach((block) => block.draw(context))
 
   player.velocity.x = 0
-  if (keysPressed.a) player.velocity.x = -4
-  else if (keysPressed.d) player.velocity.x = 4
+  if (keysPressed.a) {
+    player.switchSprite("runLeft")
+    player.velocity.x = -4
+    player.lastDirection = "left"
+  } else if (keysPressed.d) {
+    player.switchSprite("runRight")
+    player.velocity.x = 4
+    player.lastDirection = "right"
+  } else {
+    if (player.lastDirection === "left") {
+      player.switchSprite("idleLeft")
+    }
+    if (player.lastDirection === "right") {
+      player.switchSprite("idleRight")
+    }
+  }
 
   player.draw(context)
   player.update(context)
--- src/js/objects/Player.js
+++ src/js/objects/Player.js
@@ -1,8 +1,8 @@
 import Sprite from "./Sprite.js"
 
 export default class Player extends Sprite {
-  constructor({ collisionBlocks = [], imageSrc, frameRate }) {
-    super({ imageSrc, frameRate })
+  constructor({ collisionBlocks = [], imageSrc, frameRate, animations }) {
+    super({ imageSrc, frameRate, animations })
     this.position = {
       x: 200,
       y: 200,
@@ -38,6 +38,14 @@ export default class Player extends Sprite {
     this.updateHitbox()
 
     this.checkForVerticalCollisions()
+  }
+
+  switchSprite(name) {
+    if(this.image === this.animations[name].image) return
+    this.currentFrame = 0
+    this.image = this.animations[name].image
+    this.frameRate = this.animations[name].frameRate
+    this.frameBuffer = this.animations[name].frameBuffer
   }
 
   updateHitbox() {
