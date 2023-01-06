eb6389a4c911ba3eac1930c3bb90938e6a7cf20f
Atsuma <asd463291@gmail.com>
2023-01-06 10:15:37 +0800

    feat: set collision block transparent

--- src/js/objects/CollisionBlock.js
+++ src/js/objects/CollisionBlock.js
@@ -6,7 +6,7 @@ export default class CollisionBlock {
   }
 
   draw(context) {
-    context.fillStyle = 'rgba(255, 0, 0, 0.5)'
+    context.fillStyle = 'rgba(255, 0, 0, 0)'
     context.fillRect(this.position.x, this.position.y, this.width, this.height)
   }
 }
